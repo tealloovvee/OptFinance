a459e00e9a4ec6510bc4caf43d890687512e9ee2
bza16 <bza1606@gmail.com>
2025-09-15 18:27:53 +0300

    Небольшие правки

--- optfin/MainProject/asgi.py
+++ optfin/MainProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for optfin project.
+ASGI config for MainProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'optfin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MainProject.settings')
 
 application = get_asgi_application()
